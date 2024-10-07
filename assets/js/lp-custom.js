0b413e2942d220d6254be3c73e48337c88a49b16
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-10-07 09:32:30 +0700

    update file

--- assets/js/lp-custom.js
+++ assets/js/lp-custom.js
@@ -1,17 +1,7 @@
 jQuery(function($){
-    $('.faq-title').on("click", function () {
-        var $content = $(this).closest(".faq-item").find(".faq-content");
-        var $faqplus = $(this).closest(".faq-item").find(".faq-plus");
-        $(".faq-plus").removeClass("faq-add");
-        $(".faq-plus").removeClass("faq-remove");
-        $(".item-content").slideUp("200");
-        if ($content.is(":hidden")) {
-        $faqplus.addClass("faq-add");
-        $content.slideDown("200");
-        } else {
-        $content.slideUp("200");
-        $faqplus.removeClass("faq-add");
-        $faqplus.addClass("faq-remove");
-        }
-    });
+    $('.faq-title').click(function(){
+        $(this).closest(".faq-item").find(".faq-content").slideToggle();
+        $(this).closest(".faq-item").find(".faq-plus").toggleClass('faq-add');
+        return false;
+    })
 });
